f7af8cab763773520dc69348a93a5e9b2fda78d0
Alexandr Axentev <vio.aksenteval@gmail.com>
2025-02-16 22:05:15 +0300

    Добавлен кривой блок цитат

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+new Swiper('.carousel', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
